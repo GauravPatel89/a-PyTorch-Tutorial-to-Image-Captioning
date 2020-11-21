23a0b2ed157b6659e2f90b0eda75386b8bf12dfa
Gaurav Patel <gaurav4664@gmail.com>
2020-11-21 15:06:22 +0530

    Update train2.py

--- train2.py
+++ train2.py
@@ -23,7 +23,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets de
 cudnn.benchmark = True  # set to true only if inputs to model are fixed size; otherwise lot of computational overhead
 
 # Training parameters
-start_epoch = 0
+start_epoch = 42
 epochs = 120  # number of epochs to train for (if early stopping is not triggered)
 epochs_since_improvement = 0  # keeps track of number of epochs since there's been an improvement in validation BLEU
 batch_size = 32
@@ -34,7 +34,7 @@ grad_clip = 5.  # clip gradients at an absolute value of
 alpha_c = 1.  # regularization parameter for 'doubly stochastic attention', as in the paper
 best_bleu4 = 0.  # BLEU-4 score right now
 fine_tune_encoder = False  # fine-tune encoder?
-checkpoint = None  # path to checkpoint, None if none
+checkpoint = '/content/drive/MyDrive/checkpoint_flickr8k_5_cap_per_img_5_min_word_freq.pth.tar'  # path to checkpoint, None if none
 print_freq = 100
 
 
